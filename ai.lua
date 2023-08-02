03fc4c4655646e2634a79e6e018bd73449408ce8
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-08-02 18:07:31 +0530

    Add AI opponent

---
+++ ai.lua
@@ -0,0 +1,35 @@
+AI = {}
+
+function AI:load()
+    self.width = 20
+    self.height = 100
+    self.x = love.graphics.getWidth() - self.width - 50
+    self.y = love.graphics.getHeight() / 2
+    self.yVel = 0
+    self.speed = 500 --same speed as player
+end
+
+function AI:update(dt)
+    self:move(dt)
+    self:acquireTarget()
+end
+
+function AI:move(dt)
+    self.y = self.y + self.yVel /8 dt
+end
+
+function AI:acquireTarget()
+    --if the ball is above the paddle, it will move upwards
+    if Ball.y + Ball.height < self.y then
+        self.yVel = -self.yVel
+    
+    --if the ball is below the paddle, it will move downwards
+    elseif Ball.y > self,y + self.height then
+        self.yVel - self.speed
+    else
+        self.yVel = 0
+end
+
+function AI:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
--- main.lua
+++ main.lua
@@ -1,19 +1,23 @@
 require("player") --importing player file
 require("ball") --importing ball file
+require("ai") --importing ai file
 
 function love.load()
     Player:load()
     Ball:load()
+    AI:load()
 end
 
 function love.update(dt)
     Player:update(dt)
     Ball:update(dt)
+    AI:update(dt)
 end
 
 function love.draw()
     Player:draw()
     Ball:draw()
+    AI:draw()
 end
 
 function checkCollision(a, b)
